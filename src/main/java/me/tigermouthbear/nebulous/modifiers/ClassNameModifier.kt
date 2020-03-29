f2901f98124f087c5f9162caea4d7ede09381f89
Tigermouthbear <tigermouthbear@gmail.com>
2020-03-29 10:45:38 -0400

    cleaned classname modifier

--- src/main/java/me/tigermouthbear/nebulous/modifiers/ClassNameModifier.kt
+++ src/main/java/me/tigermouthbear/nebulous/modifiers/ClassNameModifier.kt
@@ -7,7 +7,7 @@ import java.util.*
 class ClassNameModifier : Modifier() {
     override fun modify() {
         val remap: MutableMap<String?, String?> = HashMap()
-        classes.forEach { classNode: ClassNode -> remap[classNode.name] = "AresClientOnTop" + RandomString.genRandomString() }
+        classes.forEach { classNode: ClassNode -> remap[classNode.name] = RandomString.genRandomString() }
         applyRemap(remap)
     }
 }
